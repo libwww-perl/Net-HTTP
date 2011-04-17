11becbc9188e3b22cd23be1e8982d823c680a6bd
Gisle Aas <gisle@aas.no>
2011-04-17 13:23:37 +0200

    Release 6.01

--- lib/Net/HTTP.pm
+++ lib/Net/HTTP.pm
@@ -3,7 +3,7 @@ package Net::HTTP;
 use strict;
 use vars qw($VERSION @ISA $SOCKET_CLASS);
 
-$VERSION = "6.00";
+$VERSION = "6.01";
 unless ($SOCKET_CLASS) {
     eval { require IO::Socket::INET } || require IO::Socket;
     $SOCKET_CLASS = "IO::Socket::INET";
